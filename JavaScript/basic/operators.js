d3f1464a0ed955df8b2d4198517c279630ba4778
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-04-23 08:32:58 +0545

    solved the assignment

---
+++ JavaScript/basic/operators.js
@@ -0,0 +1,19 @@
+//Q1 create two constants, add them and assign to a new constants
+
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
+
+
+//answer #1
+const x =10
+const y = 200
+const sum = x + y
+console.log(sum)
+
+
+ 
+// ANSWER #2
+console.log(a === b)
