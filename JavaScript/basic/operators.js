452bf32156eeecff0a0b0811dcba46a17419887d
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-04-23 08:55:26 +0545

    solved problem #3

--- JavaScript/basic/operators.js
+++ JavaScript/basic/operators.js
@@ -2,10 +2,13 @@
 //console.log "lesser than 3" else "Greater"
 
 const randomNumber = Math.floor(Math.random() * 6)
-console.log(randomNumber)
-
 if (randomNumber < 3) {
-    console.log('lesser than 3')
-} else {
-    console.log('Greater than 3')
+    console.log(randomNumber + ' lesser than 3')
+} 
+else if (randomNumber>3){
+    console.log(randomNumber+ ' Greater than 3')
+}
+
+else{
+    console.log(randomNumber+ ' equal to 3')
 }
