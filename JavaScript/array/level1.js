bba9f32a73294bc40261896e46458c39b9964488
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-04-23 09:11:17 +0545

    Solved problem #4 of array

---
+++ JavaScript/array/level1.js
@@ -0,0 +1,19 @@
+
+const arr = [3,5,2]
+joint = arr.join("")
+//Q1 calculate length of array and console log
+//expected output is 3
+
+console.log(arr.length)
+
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+const revarr = arr.reverse()
+console.log(revarr)
+
+///Q3 join the array and console log
+// expected output 352
+
+console.log(joint)
