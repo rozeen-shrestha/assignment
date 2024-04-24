6bae8579cd0d0c8e597450cd28276a72490fa20b
rozeen-shrestha <rozeeen.shrestha@gmail.com>
2024-04-24 16:56:01 +0545

    sloved assignment level 2

--- JavaScript/array/level1.js
+++ JavaScript/array/level1.js
@@ -1,6 +1,6 @@
 
 const arr = [3,5,2]
-joint = arr.join("")
+const temparr = [...arr]
 //Q1 calculate length of array and console log
 //expected output is 3
 
@@ -10,10 +10,34 @@ console.log(arr.length)
 
 //Q2 reverse the array and console log
 //expected output [2,5,3]
-const revarr = arr.reverse()
-console.log(revarr)
+
+console.log(temparr.reverse(""))
 
 ///Q3 join the array and console log
 // expected output 352
 
-console.log(joint)
+console.log(arr.join(""))
+
+//Q4 Remove the last element from the array and log the modified array to the console
+console.log(arr.slice(0, arr.length-1))
+ 
+
+
+// Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,2,8,10,12]
+let result = "All even"
+num.forEach((item)=>{
+  if (item%2 !== 0){
+    result = "Not all even"
+  }
+})
+console.log(result)
+
+
+
+// Find the index of a specific element in the array. Log the index to the console. 
+//expected output: 3
+
+const allNum = [3, 5, 2, 8, 5];
+const searchElement = 2;
+console.log(allNum.indexOf(searchElement))
--- JavaScript/array/level2.js
+++ JavaScript/array/level2.js
@@ -1,4 +1,21 @@
 const arr =  [3,5,2]
 //console.log the maximum number of an array
-//expectedd output 5c
+//expectedd output 5
 console.log(Math.max(...arr))
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+
+const elementToRemove = 2;
+console.log(arr.filter(item => item !== elementToRemove))
+
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+console.log(arr.map((item)=>{return item*5}))
+
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]
+console.log(arr.filter(item => arr2.includes(item)))
